ae78b36287d74a57d1dcc83d9a2759eddc3620d8
YAOTEKATL <yaotekatl0512@gmail.com>
2024-09-10 11:18:27 -0600

    subimos proyecto para practicar git merge

---
+++ inicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="inicio.css">
+    <title>DEP. ACT. CULTURALES Y DEPORTIVAS</title>
+</head>
+<body>
+<div>
+     <header class="header">
+        <center><a href="inicio.php"><img title="ACCESO" src="img/tesi.jpg" alt=""></a></center>
+        <center><h1>BIENVENIDO AL DEPARTAMENTO DE ACTIVIDADES CULTURALES Y DEPORTIVAS</h1></center>
+    </header>
+
+    <div>
+    <form method="post" action="procesar.php">
+        <br><center><img class="pequeña" src="img/rocko.jpg" alt=""></center></br>
+        <br><p>SELECCIONA TU ROL:</p></br>
+        <center><input type="submit" name="rol" value="Administrativo"></center>
+        <center><input type="submit" name="rol" value="Alumno"></center>
+    </form>
+    </div>
+</div>
+</body>
+</html>
